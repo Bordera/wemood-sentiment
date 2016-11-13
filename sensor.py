d9b9a152edcf53fa96a2cf06b704d4f8921ea616
Ricardo Alanis <ricardo.alanis@gmail.com>
2016-11-13 12:03:29 -0600

    Changes to connect to mongo

--- sensor.py
+++ sensor.py
@@ -3,11 +3,15 @@ import sys
 
 from subprocess import check_output
 from datetime import datetime as dt
+from pymongo import MongoClient
 
 import tools
 import record
 import transcribe
 import sentiment
+
+client = MongoClient()
+db = client.wemood
 
 # record audio
 
@@ -24,6 +28,7 @@ sentiment_output = sentiment.get_text_sentiment(sound_output["results"][0]["alte
 output_data["sentiment_data"] = sentiment_output
 output_data["sentiment_score"] = sentiment_output['docSentiment']['score']
 
+db.sensors.insert_one(clean_program_data)
 
 # extract sentiment
 with open('output/output.json', 'w') as outfile:
